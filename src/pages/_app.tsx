f28fade4dcf36753348d893d955cab5cd1c06fbf
Nathan Rothenberger <nroth10@gmail.com>
2022-12-09 23:34:30 -0600

    setup chakra + theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../theme";
 
 function MyApp({
   Component,
@@ -17,7 +19,9 @@ function MyApp({
       supabaseClient={supabase}
       initialSession={pageProps.initialSession}
     >
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionContextProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,34 @@
+import { Styles } from "@chakra-ui/theme-tools";
+import { extendTheme } from "@chakra-ui/react";
+
+export const styles: Styles = {
+  global: {
+    body: {
+      fontFamily: "body",
+      //   color: "chakra-body-bg",
+      color: "red",
+      bg: "chakra-body-text",
+      transitionProperty: "background-color",
+      transitionDuration: "normal",
+      lineHeight: "base"
+    },
+    "*::placeholder": {
+      color: "chakra-placeholder-color"
+    },
+    "*, *::before, &::after": {
+      borderColor: "chakra-border-color",
+      wordWrap: "break-word"
+    }
+  }
+};
+
+// 2. Extend the theme to include custom colors, fonts, etc
+// const colors = {
+//   brand: {
+//     900: '#1a365d',
+//     800: '#153e75',
+//     700: '#2a69ac',
+//   },
+// }
+
+export const theme = extendTheme({ styles });
