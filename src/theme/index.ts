29317c19fc7f93b20a4be0e70e5a20232cc78ce5
Nathan Rothenberger <nroth10@gmail.com>
2023-01-22 10:59:26 -0600

    WIP: theme stuff

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,35 @@
 import { Styles } from "@chakra-ui/theme-tools";
 import { extendTheme } from "@chakra-ui/react";
 
+import { defineStyleConfig } from "@chakra-ui/react";
+
+const Button = defineStyleConfig({
+  // The styles all button have in common
+  baseStyle: {
+    border: "2px solid",
+    // fontWeight: "bold",
+    // textTransform: "uppercase",
+    borderRadius: "none", // <-- border radius is same for all variants and sizes
+  },
+
+  // Two sizes: sm and md
+  sizes: {
+    md: {
+      // Font styles here?
+      fontSize: "md",
+      lineHeight: "1",
+      px: 1,
+      py: 1,
+      h: "7",
+      // minW: "10",
+    },
+  },
+  defaultProps: {
+    size: "md",
+    // variant: "outline",
+  },
+});
+
 export const styles: Styles = {
   global: {
     body: {
@@ -9,16 +38,16 @@ export const styles: Styles = {
       // bg: "chakra-body-text",
       transitionProperty: "background-color",
       transitionDuration: "normal",
-      lineHeight: "base"
+      lineHeight: "base",
     },
     "*::placeholder": {
-      color: "chakra-placeholder-color"
+      color: "chakra-placeholder-color",
     },
     "*, *::before, &::after": {
       borderColor: "chakra-border-color",
-      wordWrap: "break-word"
-    }
-  }
+      wordWrap: "break-word",
+    },
+  },
 };
 
 // 2. Extend the theme to include custom colors, fonts, etc
@@ -30,4 +59,9 @@ export const styles: Styles = {
 //   },
 // }
 
-export const theme = extendTheme({ styles });
+export const theme = extendTheme({
+  styles,
+  components: {
+    // Button,
+  },
+});
