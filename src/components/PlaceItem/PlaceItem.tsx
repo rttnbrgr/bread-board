619cd3d151f64e51def07a369558d4ed0d28364a
Nathan Rothenberger <nroth10@gmail.com>
2023-01-28 11:41:20 -0600

    cleanup place item

--- src/components/PlaceItem/PlaceItem.tsx
+++ src/components/PlaceItem/PlaceItem.tsx
@@ -1,27 +1,12 @@
-import { Box, Text, IconButton } from "@chakra-ui/react";
-import { SmallCloseIcon } from "@chakra-ui/icons";
+import { Box, Text, BoxProps } from "@chakra-ui/react";
 import React from "react";
 
-export type PlaceItemProps = {
-  onClick?: (e?: any) => void;
-  children: React.ReactNode;
-};
-
-export const PlaceItem = ({ children, onClick }: PlaceItemProps) => {
+export const PlaceItem = ({ children }: BoxProps) => {
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between">
       <Text bg="black" color="white" py="2" px="2">
         {children}
       </Text>
-      {onClick && (
-        <IconButton
-          aria-label="Remove"
-          variant="ghost"
-          colorScheme="gray"
-          onClick={onClick}
-          icon={<SmallCloseIcon />}
-        />
-      )}
     </Box>
   );
 };
